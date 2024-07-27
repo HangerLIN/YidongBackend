e5259a023599aa3c1fff1d5d5a986af4e0734a81
842480936@qq.com <842480936@qq.com>
2024-07-27 10:05:01 +0800

    开支模块——基准库表的entity

---
+++ snowy-plugin/snowy-plugin-biz/src/main/java/vip/xiaonuo/biz/modular/spend/service/impl/BenchmarkServiceImpl.java
@@ -0,0 +1,22 @@
+package vip.xiaonuo.biz.modular.spend.service.impl;
+
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import vip.xiaonuo.biz.modular.spend.entity.Benchmark;
+import vip.xiaonuo.biz.modular.spend.service.BenchmarkService;
+import vip.xiaonuo.biz.modular.spend.mapper.BenchmarkMapper;
+import org.springframework.stereotype.Service;
+
+/**
+* @author admin
+* @description 针对表【benchmark】的数据库操作Service实现
+* @createDate 2024-07-27 10:01:44
+*/
+@Service
+public class BenchmarkServiceImpl extends ServiceImpl<BenchmarkMapper, Benchmark>
+    implements BenchmarkService{
+
+}
+
+
+
+
