14a5eea32138802aa5e11cea5a4059f0a210c0c9
842480936@qq.com <842480936@qq.com>
2024-08-22 10:23:04 +0800

    New——subtemplate的输入输出模版

---
+++ snowy-plugin/snowy-plugin-biz/src/main/java/vip/xiaonuo/biz/modular/New/subtemplate/vo/SubtemplateOutput.java
@@ -0,0 +1,30 @@
+package vip.xiaonuo.biz.modular.New.subtemplate.vo;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+@NoArgsConstructor
+@Data
+public class SubtemplateOutput {
+
+    private List<SubmouleDVList> submouleDVList;
+
+    @NoArgsConstructor
+    @Data
+    public static class SubmouleDVList {
+        private Integer subtemplateId;
+        private Integer projectId;
+        private List<Data> data;
+
+        @NoArgsConstructor
+        @lombok.Data
+        public static class Data {
+            private List<String> basicInformation;
+            private BigDecimal startYearData;
+            private List<BigDecimal> endYearData;
+        }
+    }
+}
